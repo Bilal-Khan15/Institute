61877b370e48c5af25c42c296efbf0eab16a89c0
Muhammad Bilal Khan <honestbilal15@gmail.com>
2019-08-01 15:59:51 +0500

    files

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var admin = require("firebase-admin");
 
+
 // var serviceAccount = require("serviceAccountKey.json");
 var serviceAccount = require("service_account_key.json");
 
@@ -14,6 +15,59 @@ admin.initializeApp({
 const db = admin.firestore();
 
 var bucket = admin.storage().bucket();
+
+
+
+
+
+
+
+
+
+
+
+// // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+// const functions = require('firebase-functions');
+
+// // Initialize Algolia, requires installing Algolia dependencies:
+// // https://www.algolia.com/doc/api-client/javascript/getting-started/#install
+// //
+// // App ID and API Key are stored in functions config variables
+// const ALGOLIA_ID = functions.config().algolia.app_id;
+// const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
+// const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_key;
+
+// const ALGOLIA_INDEX_NAME = 'notes';
+// const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+
+
+// // Update the search index every time a blog post is written.
+// exports.onNoteCreated = functions.firestore.document('notes/{noteId}').onCreate((snap, context) => {
+//     // Get the note document
+//     const note = snap.data();
+  
+//     // Add an 'objectID' field which Algolia requires
+//     note.objectID = context.params.noteId;
+  
+//     // Write to the algolia index
+//     const index = client.initIndex(ALGOLIA_INDEX_NAME);
+//     return index.saveObject(note);
+//   });
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {
     db: db,
@@ -38,3 +92,5 @@ module.exports = {
 //     console.log(`Media link: ${metadata.mediaLink}`);
 // }
 // start()
+
+
